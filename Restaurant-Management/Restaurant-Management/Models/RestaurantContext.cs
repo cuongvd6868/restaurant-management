bd5d9ef0198043fb4cd4dde5831ce8a20453c4bb
quangminh3107 <160588768+quangminh3107@users.noreply.github.com>
2025-02-26 08:42:04 +0700

    homeok

--- Restaurant-Management/Restaurant-Management/Models/RestaurantContext.cs
+++ Restaurant-Management/Restaurant-Management/Models/RestaurantContext.cs
@@ -39,8 +39,7 @@ public partial class RestaurantContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=Restaurant;User Id=sa;Password=Minh@3107;TrustServerCertificate=True;");
-
+        
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CartItem>(entity =>
