04bd7d73b87ba00edb580cc2a6d03d0fddb084ba
quangminh3107 <160588768+quangminh3107@users.noreply.github.com>
2025-02-26 08:22:07 +0700

    giaodien

---
+++ Restaurant-Management/Restaurant-Management/Models/CartItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant_Management.Models;
+
+public partial class CartItem
+{
+    public int CartItemId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int FoodId { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public virtual Food Food { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ Restaurant-Management/Restaurant-Management/Models/FoodFavorite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant_Management.Models;
+
+public partial class FoodFavorite
+{
+    public int FoodFavoriteId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int FoodId { get; set; }
+
+    public virtual Food Food { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
