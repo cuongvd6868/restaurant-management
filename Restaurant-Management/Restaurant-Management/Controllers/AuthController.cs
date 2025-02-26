92c782db53bf309cb73ca2bf22111229c622bd19
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2025-02-26 09:17:00 +0700

    loginpage

---
+++ Restaurant-Management/Restaurant-Management/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_Management.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
