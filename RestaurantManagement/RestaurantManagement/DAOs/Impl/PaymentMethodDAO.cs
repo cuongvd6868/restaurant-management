880a74ba68ae54acc2f2b81e8c8b9986320478c0
quangminh3107 <160588768+quangminh3107@users.noreply.github.com>
2025-03-18 07:33:18 +0700

    kk

---
+++ RestaurantManagement/RestaurantManagement/DAOs/Impl/PaymentMethodDAO.cs
@@ -0,0 +1,11 @@
+﻿using RestaurantManagement.Models;
+
+namespace RestaurantManagement.DAOs.Impl
+{
+    public class PaymentMethodDAO : GenericDAO<PaymentMethod>, IPaymentMethodDAO
+    {
+        public PaymentMethodDAO(FoodDbContext context) : base(context)
+        {
+        }
+    }
+}
--- RestaurantManagement/RestaurantManagement/Repositories/Impl/FoodCategoryRepository.cs
+++ RestaurantManagement/RestaurantManagement/Repositories/Impl/FoodCategoryRepository.cs
@@ -3,7 +3,7 @@ using RestaurantManagement.Models;
 
 namespace RestaurantManagement.Repositories.Impl
 {
-    public class FoodCategoryRepository : GenericRepository<FoodCategory>,IFoodCategoryRepository
+    public class FoodCategoryRepository : GenericRepository<FoodCategory>, IFoodCategoryRepository
     {
         public FoodCategoryRepository(IGenericDAO<FoodCategory> FoodCategory) : base(FoodCategory)
         {
