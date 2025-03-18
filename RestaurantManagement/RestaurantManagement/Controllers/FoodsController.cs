30504b8fc548184386173dd6b1342e49f724b4b3
quangminh3107 <160588768+quangminh3107@users.noreply.github.com>
2025-03-18 21:53:48 +0700

    commit api food

---
+++ RestaurantManagement/RestaurantManagement/Controllers/FoodsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantManagement.DAOs;
+using RestaurantManagement.DAOs.Impl;
+using RestaurantManagement.DTOs;
+using RestaurantManagement.Models;
+using RestaurantManagement.Services;
+
+namespace RestaurantManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodsController : ControllerBase
+    {
+        private IFoodService foodService;
+
+        public FoodsController(IFoodService foodService)
+        {
+            this.foodService = foodService;
+        }
+        [Route("GetFooodsAsync")]
+        public async Task<PagedList> GetFooodsAsync(int? cateId, string? search, int pageNumber, int pageSize)
+        {
+            return await foodService.GetFooodsAsync(cateId, search, pageNumber, pageSize);
+        }
+        [Route("GetFoodCategories")]
+        public async Task<List<FoodCategory>> GetFoodCategories()
+        {
+            return await foodService.GetFoodCategories();
+        }
+    }
+}
---
+++ RestaurantManagement/RestaurantManagement/DTOs/PagedList.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantManagement.Models;
+
+namespace RestaurantManagement.DTOs
+{
+    public class PagedList
+    {
+        public List<Food> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
