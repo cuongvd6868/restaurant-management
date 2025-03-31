032c3eb15c967fb017e2980e77beeb2af3709c9b
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2025-03-31 22:40:32 +0700

    edit profile lo

---
+++ RestaurantManagement/RestaurantManagement/DTOs/UserEdit.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantManagement.DTOs
+{
+    public class UserEdit
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
